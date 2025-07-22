a7ce4a266cebea1104ca44011f52629994f1464d
MohanSaiPK <86645629+MohanSaiPK@users.noreply.github.com>
2025-07-22 20:41:12 +0530

    fixed base Url

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://the-society.onrender.com/",
+  baseURL: "https://the-society.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
